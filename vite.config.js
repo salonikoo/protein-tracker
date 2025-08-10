64e3c81c5203c87d37fdd248800b6d654bf4f0df
salonikoo <94130577+salonikoo@users.noreply.github.com>
2025-08-10 11:33:41 -1000

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT: If your repository name is different from "protein-tracker",
-// replace the base path to match your repo name: "/<your-repo-name>/"
-export default defineConfig({
-  plugins: [react()],
-  base: "/protein-tracker/",
-})
